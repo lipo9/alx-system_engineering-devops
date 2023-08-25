1cd2b7e4813ad6409665a486f2d84699e1977be6
Abderrahim Libourki <abdolibourki@gmail.com>
2023-08-25 14:58:25 +0100

    0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,17 @@
+# Create a file in /tmp
+# 
+# Requirements:
+#	File path is /tmp/school
+#	File permission is 0744
+#	File owner is www-data
+#	File group is www-data
+#	File contains I love Puppet
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
